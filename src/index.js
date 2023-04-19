9359bb3c3969989fe76bc350e9f3c30a12f6dc31
MarcinNieradka <nieradka.marcin@gmail.com>
2023-04-19 19:07:25 +0200

    template

--- src/index.js
+++ src/index.js
@@ -2,3 +2,7 @@ import Notiflix from 'notiflix';
 import axios from 'axios';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const searchEl = document.getElementById('search-form');
+const galleryEl = document.querySelector('.gallery');
+const loadBtn = document.querySelector('.load-more');
